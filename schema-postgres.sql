70c7a33d26f1e8ae51515bdc4d2758073b20a6b4
YASH\vishal.kumkar <vishal.kumkar@yash.com>
2019-01-02 12:50:49 +0530

    pom properties

--- schema-postgres.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS feedback;
-create table feedback(id serial, source varchar(255), rating integer, description varchar(255), user_name varchar(255), PRIMARY KEY (id));
-insert into  feedback( id , source,  rating , description , user_name) 
-values (1, 'yash', 5, 'na', 'usr123');
-insert into  feedback( id , source,  rating , description , user_name) 
-values (2, 'yash', 4, 'na', 'usr111');
